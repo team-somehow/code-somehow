d35b6735af6a9fe94e8a4b6fb15e3b4386bd78c8
Vinay <vinaykanse9451@gmail.com>
2023-02-05 00:42:25 +0530

    :sparkles: feat: add documentations for languages

---
+++ constants/documentations.js
@@ -0,0 +1,30 @@
+const documentations = [
+  {
+    title: "React Native - Get Started",
+    link: "https://reactnative.dev/docs/getting-started",
+  },
+  {
+    title: "React - Get Started",
+    link: "https://reactjs.org/docs/getting-started.html",
+  },
+  {
+    title: "JavaScript W3 Schools - Get Started",
+    link: "https://www.w3schools.com/js/default.asp",
+  },
+  {
+    title: "Python W3 Schools - Get Started",
+    link: "https://www.w3schools.com/python/default.asp",
+  },
+  {
+    title: "Python 3 Docs",
+    link: "https://docs.python.org/3/",
+  },
+  {
+    title: "Java W3 Schools - Get Started",
+    link: "https://www.w3schools.com/java/default.asp",
+  },
+  {
+    title: "Java Documentation",
+    link: "https://docs.oracle.com/en/java/",
+  },
+];
