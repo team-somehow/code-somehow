b958d1c4585e5d51f3c525dd1d61fa82890be354
Vinay <vinaykanse9451@gmail.com>
2023-02-05 04:19:06 +0530

    :sparkles: feat: add styling to Resource and more documentation data

--- pages/Resource.js
+++ pages/Resource.js
@@ -11,14 +11,15 @@ import documentations from "../constants/documentations";
 
 const Resource = () => {
   return (
-    <View>
-      <Text>Resource</Text>
+    <View style={styles.container}>
+      <Text style={styles.heading}>Resources</Text>
       <FlatList
+        style={styles.card}
         data={documentations}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-          <TouchableOpacity onPressed={() => Linking.openURL(item.link)}>
-            <Text>{item.title}</Text>
+          <TouchableOpacity onPress={() => Linking.openURL(item.link)}>
+            <Text style={styles.linkTitle}>{item.title}</Text>
           </TouchableOpacity>
         )}
       />
@@ -26,6 +27,33 @@ const Resource = () => {
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: "#16171a",
+    alignItems: "center",
+  },
+  card: {
+    backgroundColor: "#202226",
+    padding: 16,
+    width: "100%",
+    borderRadius: 8,
+    elevation: 64,
+    flexDirection: "row",
+    marginBottom: 16,
+  },
+  heading: {
+    color: "#D7E2FF",
+    fontSize: 18,
+    fontWeight: "900",
+    marginBottom: 16,
+  },
+  linkTitle: {
+    color: "white",
+    marginVertical: 10,
+    fontSize: 16,
+  },
+});
 
 export default Resource;
