ff3080eb89a731c363bbe35cea53da4a53f5f3d5
Vinay <vinaykanse9451@gmail.com>
2023-02-05 02:44:29 +0530

    :sparkles: feat: Add flatIcon to Resource

--- constants/documentations.js
+++ constants/documentations.js
@@ -1,29 +1,36 @@
-const documentations = [
+export default documentations = [
   {
+    id: 0,
     title: "React Native - Get Started",
     link: "https://reactnative.dev/docs/getting-started",
   },
   {
+    id: 1,
     title: "React - Get Started",
     link: "https://reactjs.org/docs/getting-started.html",
   },
   {
+    id: 2,
     title: "JavaScript W3 Schools - Get Started",
     link: "https://www.w3schools.com/js/default.asp",
   },
   {
+    id: 3,
     title: "Python W3 Schools - Get Started",
     link: "https://www.w3schools.com/python/default.asp",
   },
   {
+    id: 4,
     title: "Python 3 Docs",
     link: "https://docs.python.org/3/",
   },
   {
+    id: 5,
     title: "Java W3 Schools - Get Started",
     link: "https://www.w3schools.com/java/default.asp",
   },
   {
+    id: 6,
     title: "Java Documentation",
     link: "https://docs.oracle.com/en/java/",
   },
--- pages/Resource.js
+++ pages/Resource.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import React from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  Linking,
+} from "react-native";
+import documentations from "../constants/documentations";
 
 const Resource = () => {
-    return (
-        <View>
-            <Text>Resource</Text>
-        </View>
-    );
-}
+  return (
+    <View>
+      <Text>Resource</Text>
+      <FlatList
+        data={documentations}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPressed={() => Linking.openURL(item.link)}>
+            <Text>{item.title}</Text>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
 
 export default Resource;
