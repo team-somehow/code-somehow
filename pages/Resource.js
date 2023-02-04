f68f42a1e693ccebdc07cb1e3ee81354d35a2809
Harshal Dave <harshaldave74@gmail.com>
2023-02-05 04:51:34 +0530

    :sparkles: feat: resouces ui done

--- pages/Resource.js
+++ pages/Resource.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
-  View,
-  StyleSheet,
-  Text,
-  FlatList,
   TouchableOpacity,
-  Linking,
+  StyleSheet,
+  View,
+  Text,
+  SafeAreaView,
+  StatusBar,
+  Image,
+  Dimensions,
+  ScrollView,
+  ImageBackground,
+  FlatList,
+  Alert,
 } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import {
+  SimpleLineIcons,
+  MaterialCommunityIcons,
+  FontAwesome,
+  Octicons, 
+  Feather
+} from "@expo/vector-icons";
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart,
+} from "react-native-chart-kit";
+
+const { width, height } = Dimensions.get("window");
 import documentations from "../constants/documentations";
 
 const Resource = () => {
@@ -14,12 +40,13 @@ const Resource = () => {
     <View style={styles.container}>
       <Text style={styles.heading}>Resources</Text>
       <FlatList
-        style={styles.card}
         data={documentations}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => Linking.openURL(item.link)}>
+          <TouchableOpacity onPress={() => Linking.openURL(item.link)} style={styles.card}>
             <Text style={styles.linkTitle}>{item.title}</Text>
+            {/* <Octicons name="link-external" size={24} color="#D7E2FF64" /> */}
+            <Feather name="external-link" size={24} color="#D7E2FF64" />
           </TouchableOpacity>
         )}
       />
@@ -32,22 +59,28 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
     backgroundColor: "#16171a",
-    alignItems: "center",
+    // alignItems: "center",
   },
   card: {
     backgroundColor: "#202226",
-    padding: 16,
-    width: "100%",
+    padding: 8,
+    paddingHorizontal: 24,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: width - 48,
     borderRadius: 8,
-    elevation: 64,
+    elevation: 6,
     flexDirection: "row",
     marginBottom: 16,
   },
   heading: {
+    textAlign: 'left',
     color: "#D7E2FF",
-    fontSize: 18,
-    fontWeight: "900",
-    marginBottom: 16,
+    fontSize: 24,
+    fontWeight: "600",
+    letterSpacing: 0.8,
+    marginBottom: 24,
   },
   linkTitle: {
     color: "white",
