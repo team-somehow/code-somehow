2ff444b87cafab2fb1f8b6dbce23a46041773f5c
Vinay <vinaykanse9451@gmail.com>
2023-02-05 09:44:22 +0530

    :sparkles: feat: Add more languages support

--- constants/languages.js
+++ constants/languages.js
@@ -1,12 +1,14 @@
 export const data = [
-  // {
-  //   value: 45,
-  //   label: "Assembly (NASM 2.14.02)",
-  // },
-  // {
-  //   value: 46,
-  //   label: "Bash (5.0.0)"
-  // },
+  {
+    value: 45,
+    label: "Assembly (NASM 2.14.02)",
+    ready: "asm",
+  },
+  {
+    value: 46,
+    label: "Bash (5.0.0)",
+    ready: "sh",
+  },
   // {
   //   value: 47,
   //   label: "Basic (FBC 1.07.1)"
@@ -20,23 +22,26 @@ export const data = [
   //   label: "C++ (Clang 7.0.1)"
   // },
 
-
   // {
   //   value: 48,
-  //   label: "C (GCC 7.4.0)"
+  //   label: "C (GCC 7.4.0)",
+  //   ready: "c"
   // },
   // {
   //   value: 52,
-  //   label: "C++ (GCC 7.4.0)"
+  //   label: "C++ (GCC 7.4.0)",
+  //   ready: "cpp"
   // },
 
   {
     value: 49,
-    label: "C (GCC 8.3.0)"
+    label: "C (GCC 8.3.0)",
+    ready: "c",
   },
   {
     value: 53,
-    label: "C++ (GCC 8.3.0)"
+    label: "C++ (GCC 8.3.0)",
+    ready: "cpp",
   },
 
   // {
@@ -48,15 +53,15 @@ export const data = [
   //   label: "C++ (GCC 9.2.0)"
   // },
 
-
   // {
   //   value: 86,
   //   label: "Clojure (1.10.1)"
   // },
-  // {
-  //   value: 51,
-  //   label: "C# (Mono 6.6.0.161)"
-  // },
+  {
+    value: 51,
+    label: "C# (Mono 6.6.0.161)",
+    ready: "csharp",
+  },
   // {
   //   value: 77,
   //   label: "COBOL (GnuCOBOL 2.2)"
@@ -65,34 +70,39 @@ export const data = [
   //   value: 55,
   //   label: "Common Lisp (SBCL 2.0.0)"
   // },
-  // {
-  //   value: 56,
-  //   label: "D (DMD 2.089.1)"
-  // },
-  // {
-  //   value: 57,
-  //   label: "Elixir (1.9.4)"
-  // },
-  // {
-  //   value: 58,
-  //   label: "Erlang (OTP 22.2)"
-  // },
+  {
+    value: 56,
+    label: "D (DMD 2.089.1)",
+    ready: "d",
+  },
+  {
+    value: 57,
+    label: "Elixir (1.9.4)",
+    ready: "elixir",
+  },
+  {
+    value: 58,
+    label: "Erlang (OTP 22.2)",
+    ready: "erlang",
+  },
   // {
   //   value: 44,
   //   label: "Executable"
   // },
-  // {
-  //   value: 87,
-  //   label: "F# (.NET Core SDK 3.1.202)"
-  // },
-  // {
-  //   value: 59,
-  //   label: "Fortran (GFortran 9.2.0)"
-  // },
+  {
+    value: 87,
+    label: "F# (.NET Core SDK 3.1.202)",
+    ready: "fsharp",
+  },
+  {
+    value: 59,
+    label: "Fortran (GFortran 9.2.0)",
+    ready: "fortran",
+  },
   {
     value: 60,
     label: "Go (1.13.5)",
-    ready: "go"
+    ready: "go",
   },
   // {
   //   value: 88,
@@ -105,21 +115,23 @@ export const data = [
   {
     value: 62,
     label: "Java (OpenJDK 13.0.1)",
-    ready: "java"
+    ready: "java",
   },
   {
     value: 63,
     label: "JavaScript (Node.js 12.14.0)",
-    ready: "javascript"
+    ready: "javascript",
   },
-  // {
-  //   value: 78,
-  //   label: "Kotlin (1.3.70)"
-  // },
-  // {
-  //   value: 64,
-  //   label: "Lua (5.3.5)"
-  // },
+  {
+    value: 78,
+    label: "Kotlin (1.3.70)",
+    ready: "kotlin",
+  },
+  {
+    value: 64,
+    label: "Lua (5.3.5)",
+    ready: "lua",
+  },
   // {
   //   value: 89,
   //   label: "Multi-file program"
@@ -140,14 +152,16 @@ export const data = [
   //   value: 67,
   //   label: "Pascal (FPC 3.0.4)"
   // },
-  // {
-  //   value: 85,
-  //   label: "Perl (5.28.1)"
-  // },
-  // {
-  //   value: 68,
-  //   label: "PHP (7.4.1)"
-  // },
+  {
+    value: 85,
+    label: "Perl (5.28.1)",
+    ready: "perl",
+  },
+  {
+    value: 68,
+    label: "PHP (7.4.1)",
+    ready: "php",
+  },
   // {
   //   value: 43,
   //   label: "Plain Text"
@@ -163,41 +177,46 @@ export const data = [
   {
     value: 71,
     label: "Python (3.8.1)",
-    ready: "python"
+    ready: "python",
   },
   {
     value: 80,
     label: "R (4.0.0)",
-    ready: "r"
+    ready: "r",
   },
   {
     value: 72,
     label: "Ruby (2.7.0)",
-    ready: "ruby"
+    ready: "ruby",
   },
   {
     value: 73,
     label: "Rust (1.40.0)",
-    ready: "rust"
+    ready: "rust",
   },
-  // {
-  //   value: 81,
-  //   label: "Scala (2.13.2)"
-  // },
-  // {
-  //   value: 82,
-  //   label: "SQL (SQLite 3.27.2)"
-  // },
-  // {
-  //   value: 83,
-  //   label: "Swift (5.2.3)"
-  // },
-  // {
-  //   value: 74,
-  //   label: "TypeScript (3.7.4)"
-  // },
-  // {
-  //   value: 84,
-  //   label: "Visual Basic.Net (vbnc 0.0.0.5943)"
-  // }
-]
+  {
+    value: 81,
+    label: "Scala (2.13.2)",
+    ready: "scala",
+  },
+  {
+    value: 82,
+    label: "SQL (SQLite 3.27.2)",
+    ready: "sql",
+  },
+  {
+    value: 83,
+    label: "Swift (5.2.3)",
+    ready: "swift",
+  },
+  {
+    value: 74,
+    label: "TypeScript (3.7.4)",
+    ready: "typescript",
+  },
+  {
+    value: 84,
+    label: "Visual Basic.Net (vbnc 0.0.0.5943)",
+    ready: "vbnet",
+  },
+];
